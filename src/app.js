08ac9342a61b06feee37c0a3a9340beab51d653a
luigifurlani13 <luis.furlan@icloud.com>
2021-06-28 05:53:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  createCard();
   console.log("Hello Rigo from the console!");
 };
+
+let numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let suits = ["♦", "♥", "♠", "♣"];
+
+let lastNumber = numbers.length - 1;
+let lastSuit = suits.length - 1;
+
+let randomNumber = numbers[Math.floor(Math.random() * lastNumber)];
+let randomSuits = suits[Math.floor(Math.random() * lastSuit)];
+
+function createCard() {
+  document.querySelector("#numbers").innerHTML = randomNumber;
+  document.getElementById("numbers").style.fontSize = "300px";
+
+  document.querySelector("#topSuit").innerHTML = randomSuits;
+  document.querySelector("#bottomSuit").innerHTML = randomSuits;
+  console.log(randomNumber);
+  console.log(randomSuits);
+}
