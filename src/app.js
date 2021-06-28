77d6b5346be02a86a07458d47c5a077509b509a3
luigifurlani13 <luis.furlan@icloud.com>
2021-06-28 16:43:01 +0000

    finished product

--- src/app.js
+++ src/app.js
@@ -33,13 +33,28 @@ let lastSuit = suits.length - 1;
 
 let randomNumber = numbers[Math.floor(Math.random() * lastNumber)];
 let randomSuits = suits[Math.floor(Math.random() * lastSuit)];
+let colorSuits;
+
+if (randomSuits === "♦" || randomSuits === "♥") {
+  colorSuits = "red";
+}
 
 function createCard() {
   document.querySelector("#numbers").innerHTML = randomNumber;
-  document.getElementById("numbers").style.fontSize = "300px";
+  document.getElementById("numbers").style.fontSize = "200px";
 
   document.querySelector("#topSuit").innerHTML = randomSuits;
+  document.getElementById("topSuit").style.fontSize = "100px";
+  document.getElementById("topSuit").style.color = colorSuits;
+
   document.querySelector("#bottomSuit").innerHTML = randomSuits;
-  console.log(randomNumber);
-  console.log(randomSuits);
+  document.getElementById("bottomSuit").style.fontSize = "100px";
+  document.getElementById("bottomSuit").style.color = colorSuits;
 }
+
+let btn = document.createElement("button");
+btn.innerHTML = "New Card";
+btn.addEventListener("click", function() {
+  window.location.reload();
+});
+document.body.appendChild(btn);
